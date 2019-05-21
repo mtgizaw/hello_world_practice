011f1dcee2094b073369effeb5c9be4f6c098e5a
Martha Gizaw <34733400+MGizaw1996@users.noreply.github.com>
2019-05-21 15:54:45 -0400

    Add files via upload
    Just uploaded the file in Python that uses classes, methods, and print functions

---
+++ Python Hello World.py
@@ -0,0 +1,19 @@
+# Printing Hello World!
+
+class ABC:
+    def method(self):
+        print("Hello World!")
+        
+class XYZ:
+    def another(self):
+        print(4+4+4)
+
+x = ABC()
+x.method()
+
+y = XYZ()
+y.another()
+
+print("Hello World! May I take your order?")
+
+print(8)
